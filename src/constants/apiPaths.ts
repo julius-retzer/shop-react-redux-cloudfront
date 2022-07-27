04e489758f3fa706cbfa59039dc844522e6bc4d6
Július Retzer <jul.retzer@gmail.com>
2022-07-27 10:40:43 +0200

    CSV import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://seyrirlsjd.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://seyrirlsjd.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://seyrirlsjd.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://bp0x2ycwfj.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://seyrirlsjd.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://seyrirlsjd.execute-api.us-east-1.amazonaws.com/dev",
 };
